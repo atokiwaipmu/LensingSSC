3c1a536f5c75e3b4581d8a36a37b1493098ee9ed
atokiwaipmu <akira.tokiwa@ipmu.jp>
2025-02-28 17:27:55 +0900

    Remove unused modules and obsolete plot files to clean up the repository

--- env.sh
+++
@@ -1,8 +0,0 @@
-# Conda 環境を作成
-conda create --prefix ./env python=3.8
-conda activate ./env
-
-# 必要なパッケージをインストール
-conda install -c bccp nbodykit
-conda install -c conda-forge mpi4py pyccl
-python -m pip install cobaya --upgrade
